e57d044c5649125e8fb22e73db51201aefa24094
Edthecoder254 <marvedie254@gmail.com>
2018-07-17 07:46:51 +0300

    2nd

--- app/src/main/java/com/marvedie/servicesmarketapp/HireActivity.java
+++ app/src/main/java/com/marvedie/servicesmarketapp/HireActivity.java
@@ -1,51 +1,42 @@
 package com.marvedie.servicesmarketapp;
 
-import android.support.v4.view.ViewPager;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.support.design.widget.TabLayout;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class HireActivity extends AppCompatActivity {
-
-    //create Tag
-    private static final String TAG = "HireActivity";
-    //declare SectionsPageAdapter
-    private SectionsPageAdapter mSectionsPageAdapter;
-
-    //declare ViewPager
-    private ViewPager mViewPager;
+    //Variables
+    ListView mListView;
+    List<String> mList = new ArrayList<>();
+    ArrayAdapter<String> mAdapter;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hire);
-        Log.d(TAG, "onCreate: starting,");
 
-        //declare sections adapter
-        mSectionsPageAdapter = new SectionsPageAdapter(getSupportFragmentManager());
+        //Initialise the Variables defined above
+        mListView = findViewById(R.id.lvCategories);
 
-        //Set up the ViewPager with the sections adapter
-        mViewPager = findViewById(R.id.container);
-        setupViewPager(mViewPager);
 
-        //declare TabLayout Object
-        TabLayout tabLayout = findViewById(R.id.tabs);
-        tabLayout.setupWithViewPager(mViewPager);
+        mList.add("Banana");
+        mList.add("Cow");mList.add("S.E.O writing");
+        mList.add("Goat");mList.add("Graphic Design");
+        mList.add("Bicycle");mList.add("What");
+        mList.add("Bat");mList.add("When");
+        mList.add("Lion");mList.add("How");
+        mList.add("IT");mList.add("Now");
+        mList.add("Writing");mList.add("Bat");
 
-    }
-
-    //method to create sections page adapter and add fragments to it and give titles
-    private void setupViewPager (ViewPager viewPager){
-        SectionsPageAdapter adapter = new SectionsPageAdapter(getSupportFragmentManager());
-        adapter.addFragment(new Tab1Fragment(), "TAB1");
-        adapter.addFragment(new Tab2Fragment(), "TAB2");
-        adapter.addFragment(new Tab3Fragment(), "TAB3");
-
-        //Parse adapter to the viewPager
-        viewPager.setAdapter(adapter);
+        //Initialise the Array Adapter
+        mAdapter = new ArrayAdapter<>(HireActivity.this, android.R.layout.simple_list_item_1,mList);
+        mListView.setAdapter(mAdapter);
     }
 
 }
